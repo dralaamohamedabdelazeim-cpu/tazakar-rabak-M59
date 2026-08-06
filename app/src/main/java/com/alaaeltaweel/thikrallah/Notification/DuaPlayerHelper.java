2b6957648b5403e8229b0d0aea365c307be3996a
dralaamohamedabdelazeim-cpu <dr.alaamohamedabdelazeim@gmail.com>
2026-08-07 00:09:03 +0300

    Update DuaPlayerHelper.java

--- app/src/main/java/com/alaaeltaweel/thikrallah/Notification/DuaPlayerHelper.java
+++ app/src/main/java/com/alaaeltaweel/thikrallah/Notification/DuaPlayerHelper.java
@@ -62,7 +62,7 @@ public class DuaPlayerHelper {
             duaMediaPlayer = null;
         }
         AudioManager am = (AudioManager) context.getSystemService(Context.AUDIO_SERVICE);
-      if (am != null && duaMediaPlayer != null) am.abandonAudioFocus(null);// ✅ سيب الميكروفون هنا كمان لو المستخدم وقف الدعاء يدوي
+      if (am != null) am.abandonAudioFocus(null);// ✅ سيب الميكروفون هنا كمان لو المستخدم وقف الدعاء يدوي
         android.app.NotificationManager nm = (android.app.NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
         if (nm != null) nm.cancel(9911);
     }
