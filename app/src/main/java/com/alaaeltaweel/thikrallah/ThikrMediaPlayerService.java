0e7a93f3b09f347ad50ce0ea1b2daefa99987465
dralaamohamedabdelazeim-cpu <dr.alaamohamedabdelazeim@gmail.com>
2026-09-16 20:46:52 +0300

    Update ThikrMediaPlayerService.java

--- app/src/main/java/com/alaaeltaweel/thikrallah/ThikrMediaPlayerService.java
+++ app/src/main/java/com/alaaeltaweel/thikrallah/ThikrMediaPlayerService.java
@@ -507,11 +507,11 @@ public class ThikrMediaPlayerService extends Service implements OnCompletionList
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
 
-            String NOTIFICATION_CHANNEL_ID = "ThikrMediaPlayerService_v2";
+            String NOTIFICATION_CHANNEL_ID = "ThikrMediaPlayerService";
 
             String channelName = this.getResources().getString(R.string.remember_notification);
 
-            NotificationChannel chan = new NotificationChannel(NOTIFICATION_CHANNEL_ID, channelName, NotificationManager.IMPORTANCE_LOW);
+            NotificationChannel chan = new NotificationChannel(NOTIFICATION_CHANNEL_ID, channelName, NotificationManager.IMPORTANCE_DEFAULT);
 
             chan.setSound(null, new AudioAttributes.Builder()
 
